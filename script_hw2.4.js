202f62635df94b59c21423b736cc58fcc314bc1b
IrinaTrukhina <irin-trukhina@yandex.ru>
2024-01-31 01:17:24 +0300

    Создала файл script_hw2.4.js, сделала задания 1-3

---
+++ script_hw2.4.js
@@ -0,0 +1,23 @@
+//Задание 1
+let i = 1;
+let n = 2;
+while (i <= n) {
+    alert('Привет!');
+    i++;
+}
+
+//Задание 2
+let i = 1;
+let n = 5;
+while (i <= n) {
+    console.log(i);
+    i++;
+}
+
+//Задание 3
+let i = 7;
+let n = 27;
+while (i <= n) {
+    console.log(i);
+    i++;
+}
