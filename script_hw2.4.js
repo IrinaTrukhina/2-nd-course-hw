ee47782af083c6103ceb7f24411895f385375ab3
IrinaTrukhina <irin-trukhina@yandex.ru>
2024-02-04 01:36:22 +0300

    Направляю ДЗ к уроку 2.4.

--- script_hw2.4.js
+++ script_hw2.4.js
@@ -21,3 +21,30 @@ while (i <= n) {
     console.log(i);
     i++;
 }
+//Задание 4
+const obj = {
+    'Коля': '200',
+    'Вася': '300',
+    'Петя': '400',
+};
+for (let name in obj) {
+        console.log(`${name} - зарплата ${obj[name]}`);
+}
+
+
+//Задание 5
+let n = 1000; // задаем начальное значение счетчика
+let num = 1;
+while(n > 50) { // проверяем, что счетчик больше 50
+    n /= 2; console.log(`Итерация № ${num++}, результат этой итерациии будет ${n}`);
+}
+
+
+//Задание 6
+for (let dayInMonth = 1; dayInMonth <= 31; dayInMonth ++) {
+    if (dayInMonth == 5 || dayInMonth == 12 || dayInMonth == 19 || dayInMonth == 26 ) {
+    console.log(`Сегодня пятница, ${dayInMonth}е число. Необходимо подготовить отчет.`);
+    } else if (dayInMonth !== 5 || dayInMonth !== 12 || dayInMonth == !19 || dayInMonth == !26 ) {
+    console.log(`Сегодня не пятница, ${dayInMonth}е число. Отчет не нужен.`);
+    }
+}
